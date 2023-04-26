c82b8f050ae9318140232c505c526f57e29ff4cd
Saša Banjavčić <sasa.banjavcic@gmail.com>
2023-04-26 20:15:48 +0200

    Simplified randomName.js file

--- src/utils/randomName.js
+++ src/utils/randomName.js
@@ -1,17 +1,7 @@
-const usedNames = new Set()
-
 export const randomName = () => {
   const adjectives = ['Brzi', 'Ljuti', 'Gladan', 'Zgodan', 'Morski', 'Divlji']
   const nouns = ['Mačak', 'Zec', 'Ris', 'Vuk', 'Galeb', 'Poskok', 'Tuljan']
-  const randomIndex = (array) => Math.floor(Math.random() * array.length)
+  const randomIndex = () => Math.floor(Math.random() * adjectives.length)
 
-  let adjective, noun, name
-  do {
-    adjective = adjectives[randomIndex(adjectives)]
-    noun = nouns[randomIndex(nouns)]
-    name = `${adjective} ${noun}`
-  } while (usedNames.has(name))
-
-  usedNames.add(name)
-  return name
+  return `${adjectives[randomIndex()]} ${nouns[randomIndex()]}`
 }
